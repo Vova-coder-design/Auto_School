568113decddf673d4f5ceb5fed11452522893c0d
Vova-coder-design <65913397+Vova-coder-design@users.noreply.github.com>
2020-06-17 15:57:02 +0300

    Добавьте файлы проекта.

---
+++ Auto__School/Connection_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto__School
+{
+    public partial class Connection_Form : Form
+    {
+        public Connection_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
